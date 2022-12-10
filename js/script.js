d70a7f0f18f075accffb2c545aecf3e095d03eb2
movieaddict.github.io <119567247+movieaddictt@users.noreply.github.com>
2022-12-09 19:25:07 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+ 
+  });
